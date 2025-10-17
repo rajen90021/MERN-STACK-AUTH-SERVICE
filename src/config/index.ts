ce9b42d06babd50b04b5a5905223542beaecd6ed
rajen90021 <grajen90021@gmail.com>
2025-10-17 11:48:37 +0530

    added vode

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,11 +1,10 @@
-import {config} from 'dotenv'
+import { config } from 'dotenv'
 
 config()
 
+const { PORT, NODE_ENV } = process.env
 
-const {PORT ,NODE_ENV}= process.env
-
-export const Config ={
-        PORT,
-        NODE_ENV
+export const Config = {
+  PORT,
+  NODE_ENV,
 }
--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,17 @@
-import { Config } from "./config";
+import app from './app'
+import { Config } from './config'
+import logger from './config/logger'
 
+export const startServer = () => {
+  const port = Config.PORT
 
-console.log(Config.PORT)
+  try {
+    app.listen(port, () => {
+      logger.info("app is listening",{port :port})
+    })
+  } catch (e) {
+    console.log(e)
+  }
+}
+
+startServer()
