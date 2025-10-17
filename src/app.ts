e3918253e2348ff5793f9b04db64bac4b5e73124
rajen90021 <grajen90021@gmail.com>
2025-10-17 12:21:48 +0530

    added global error handler

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,42 @@
-import express from 'express'
+import express, { NextFunction, Request, Response } from 'express'
+import logger from './config/logger'
+import createHttpError, { HttpError } from 'http-errors'
 
 const app = express()
 
-app.get('/', (req, res) => {
-  res.send('hellow this is tersting ')
+app.get('/',  (req, res, next) => {
+
+  const err = createHttpError(401,"you cannot access this route")
+  next(err)
+  
 })
+
+
+// global error handler 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+app.use((err: HttpError ,req:Request ,res :Response ,next:NextFunction)=>{
+
+    logger.error(`[${req.method}] ${req.url} - ${err.message}`)
+    logger.error(err.message)
+    const statusCode = err.statusCode || 500
+    res.status(statusCode).json({
+
+      errors :[
+        {
+          type:err.name,
+          msg:err.message,
+          path:"",
+          location:""
+        }
+      ]
+    })
+
+
+})
+
+
+
+
+
 
 export default app
